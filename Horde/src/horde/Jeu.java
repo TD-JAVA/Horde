2aabf480cd3af6b6dfd5ab97ba2b11fa61bf9888
0neiroi <valere.richier@gmail.com>
2015-11-17 11:33:06 +0100

    Ajout de classe

---
+++ Horde/src/horde/Jeu.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horde;
+
+/**
+ *
+ * @author oneiroi
+ */
+public class Jeu {
+    // C'est un commentaire le temps de créer la classe joueur
+    /*private Joueur[] tabJoueur= new Joueur[10];
+    public Joueur getJoueur(){
+        return tabJoueur;
+    }
+    
+    public void setJoueur(Joueur citoyen){
+    
+    }*/
+    
+    public void initialisation(){
+        
+    
+    }
+    
+    public void finDePartie(){
+    
+    
+    }
+}
