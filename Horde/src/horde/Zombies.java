df75ee9da2695fa7e3e9abbcf4ab04848124cc09
Gab55 <gabrielgiroud@gmail.com>
2015-11-17 11:48:55 +0100

    classe Zombies
    Atrib + constructeur

---
+++ Horde/src/horde/Zombies.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horde;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Zombies {
+    int nbZombies;
+    int listeJoueursMort;
+
+    public Zombies(int nbZombies, int listeJoueursMort) {
+        this.nbZombies = nbZombies;
+        this.listeJoueursMort = listeJoueursMort;
+    }
+    
+    
+    
+}
