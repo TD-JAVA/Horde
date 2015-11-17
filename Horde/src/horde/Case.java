f637e36f73a8b54305974489aa357fb6d4795196
Gab55 <gabrielgiroud@gmail.com>
2015-11-17 11:08:47 +0100

    Création classe Case
    Atri + Constructor

---
+++ Horde/src/horde/Case.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horde;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Case {
+    public int longitude;
+    public int latitude;
+    public int nbZombies;
+    int[] items= new int[20]; // valeur par défault//
+    public Boolean fouiller;
+
+    public Case(int longitude, int latitude, int nbZombies, Boolean fouiller) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.nbZombies = nbZombies;
+        this.fouiller = fouiller;
+    }
+    
+    
+
+    
+}
